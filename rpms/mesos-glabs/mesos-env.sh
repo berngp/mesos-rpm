50399ab9dd274b49a8ae6bca57c22dd181e3036f
Bernardo Gomez Palacio <bernardo.gomezpalacio@gmail.com>
2013-08-28 17:26:42 -0700

    First Mesos SPEC build with mesos-master, mesos-slave, mesos-local, mesos-devel packages

---
+++ rpms/mesos-glabs/mesos-env.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This file is meant to be sourced.
+# It sets environment variables so Mesos will search for uninstalled
+# dependencies (such as the WebUI) rather than looking in installed locations.
+
+#prefix=@prefix@
+#exec_prefix=@exec_prefix@
+
+# Load external environments if available. 
+if   [ -x "$HOME/.mesos/env.sh" ] ; then
+    MESOS_ENV_SH="$HOME/.mesos/env.sh"
+elif [ -f "/etc/sysconfig/mesos.sh" ] ; then
+    MESOS_ENV_SH="/etc/sysconfig/mesos.sh"
+fi
+
+if [ -x "$MESOS_ENV_SH" ]; then
+    echo "MESOS Env Shell available at ${MESOS_ENV_SH}."
+    source "$MESOS_ENV_SH"
+fi
+
+# Account used to start the process.
+MESOS_USER=${MESOS_USER-mesos}
+MESOS_GROUP=${MESOS_GROUP-mesos}
+
+# Make sure a log file is defined.
+LOG_DIR=${LOG_DIR:="/var/log/mesos/$prog"}
+if [ ! -d "$LOG_DIR" ]; then
+    echo "Making $LOG_DIR"
+    mkdir -p "$LOG_DIR"
+    chown -R "$MESOS_USER:$MESOS_GROUP" "$LOG_DIR"
+fi
+# Setup Configuration Directory
+if [ -f "/etc/mesos/$prog.conf"  ]; then
+    CONFIGFILE="/etc/mesos/$prog.conf"
+else
+    CONFIGFILE="/etc/mesos/mesos.conf"
+fi
+# Initialize the _Command_ options.
+#OPTIONS="--log_dir=$LOG_DIR"
+OPTIONS=""
+if [ -n "$CONFIGFILE" ]; then
+    OPTIONS_F=`paste -d " " <(cat "${CONFIGFILE}" | grep -e "^[--]" )`
+    if [ -n "$OPTIONS_F" ]; then
+        OPTIONS="$OPTIONS $OPTIONS_F"
+    fi
+fi
+
+# NUMA
+# This verifies the existence of numactl as well as testing that the command works
+NUMACTL_ARGS="--interleave=all"
+if which numactl >/dev/null 2>/dev/null && numactl $NUMACTL_ARGS ls / >/dev/null 2>/dev/null
+then
+    NUMACTL="`which numactl` $NUMACTL_ARGS"
+else
+    NUMACTL=""
+fi
+
+# Make sure pid and lock files are defined.
+LOCKFILE=${LOCKFILE:-"/var/lock/subsys/$prog"}
+PIDFILE=${PIDFILE:-"/var/run/$prog.pid"}
+
+OUT_FILE="${LOG_DIR}/$prog.out"
+
---
+++ rpms/mesos-glabs/mesos-masterd.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This is a wrapper for running mesos-slave before it is installed
+# that first sets up environment variables as appropriate.
+
+
+# mesos-masterd - Startup script for Mesos Master
+
+# chkconfig: 35 85 15
+# description: 
+# processname: mesos-masterd 
+# config: /etc/mesos/mesos.conf
+# pidfile: /var/run/mesos-masterd.pid
+. /etc/rc.d/init.d/functions
+
+# NOTE: if you change any OPTIONS here, you get what you pay for:
+# this script assumes all options are in the config file.
+
+PRG="$0"
+
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+# Get standard environment variables
+PRGDIR=`dirname "$PRG"`
+
+# Establish which Command we are going to run.
+mesosd=`which mesos-master`
+if [ ! -n "$mesosd" ]; then
+   echo $"ERROR: It appears that the file $mesosd is unreachable or unexecutable."
+   exit -1
+fi
+prog=$(basename $mesosd)
+
+# Source the default environment variables.
+ENV="$PRGDIR/mesos-env.sh"
+if [ -f "$ENV" ]; then
+    source $ENV
+else
+    echo "ERROR Environment file $ENV is missing!"
+    exit -1
+fi
+
+start() {
+    [ -x $mesosd ] || exit 5
+    echo -n $"Starting Mesos Master ($mesosd):"
+    touch "$OUT_FILE"
+    if [ -n "$NUMACTL" ]; then
+        echo $"Running NUMA $NUMACTL"
+    fi
+    daemonize -a -e "$OUT_FILE" -o "$OUT_FILE" -p "$PIDFILE" -l "$LOCKFILE" -u "$MESOS_USER" $NUMACTL $mesosd $OPTIONS
+    RETVAL=$?
+    echo ""
+    [ $RETVAL -eq 0 ] && touch "$LOCKFILE"
+    return $RETVAL
+}
+
+stop() {
+    echo -n $"Stopping Mesos Master ($mesosd): "
+    killproc $prog -SIGTERM
+    RETVAL=$?
+    echo
+    [ $RETVAL -eq 0 ] && rm -f $LOCKFILE
+    return $retval
+}
+
+
+restart() {
+    stop
+    start
+}
+
+reload() {
+    echo -n $"Reloading $prog: "
+    killproc $prog -HUP
+    RETVAL=$?
+    echo ""
+}
+
+force_reload() {
+    restart
+}
+ 
+rh_status() {
+    status $prog
+}
+ 
+rh_status_q() {
+    rh_status >/dev/null 2>&1
+}
+
+ulimit -n 12000
+RETVAL=0
+
+case "$1" in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  restart|reload|force-reload)
+    restart
+    ;;
+  condrestart)
+    [ -f "$LOCKFILE" ] && restart || :
+    ;;
+  status)
+    status $mesosd
+    RETVAL=$?
+    ;;
+  *)
+    echo "Usage: $0 {start|stop|status|restart|reload|force-reload|condrestart}"
+    RETVAL=1
+esac
+
+exit $RETVAL
